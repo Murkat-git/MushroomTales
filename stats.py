e8031fabc4504139eb6a9691bb3b5b0f5c4f8093
Vadim <vpzakharov@edu.hse.ru>
2023-01-13 21:23:55 +0300

    init stats

--- stats.py
+++ stats.py
@@ -3,16 +3,119 @@ ENEMY_STATS = {
         "speed": 1,
         "hp": 3,
         "weapon": "bat"
+    },
+    "boar_": {
+        "speed": 1,
+        "hp": 3,
+        "weapon": "ironKnife"
+    },
+    "centaur_": {
+        "speed": 1,
+        "hp": 3,
+        "weapon": "goldenSpear"
+    },
+    "fairyQueen": {
+        "speed": 1,
+        "hp": 5,
+        "weapon": "goldenKnife"
+    },
+    "fFairy": {
+        "speed": 1,
+        "hp": 3,
+        "weapon": "goldenKnife"
+    },
+    "fungiant": {
+        "speed": 1,
+        "hp": 3,
+        "weapon": "ironPickaxe"
+    },
+    "gnome": {
+        "speed": 1,
+        "hp": 3,
+        "weapon": "stoneKnife"
+    },
+    "golem": {
+        "speed": 1,
+        "hp": 3,
+        "weapon": "ironSword"
+    },
+    "ogre": {
+        "speed": 1,
+        "hp": 3,
+        "weapon": "stonePickaxe"
+    },
+    "troll": {
+        "speed": 1,
+        "hp": 3,
+        "weapon": "stonePickaxe"
     }
+
 }
 
 WEAPONS_STATS = {
-    "bat":{
+    "bat": {
+        "projectile_type": "attack_wave",
+        "projectile_speed": 1.5,
+        "projectile_lifetime": 500,
+        "dmg": 1
+    },
+    "ironKnife": {
+        "projectile_type": "attack_wave",
+        "projectile_speed": 1.5,
+        "projectile_lifetime": 500,
+        "dmg": 1
+    },
+    "StonePickaxe": {
+        "projectile_type": "attack_wave",
+        "projectile_speed": 1.5,
+        "projectile_lifetime": 500,
+        "dmg": 1
+    },
+    "ironSword": {
+        "projectile_type": "attack_wave",
+        "projectile_speed": 1.5,
+        "projectile_lifetime": 500,
+        "dmg": 1
+    },
+    "stoneKnife": {
+        "projectile_type": "attack_wave",
+        "projectile_speed": 1.5,
+        "projectile_lifetime": 500,
+        "dmg": 1
+    },
+    "goldenKnife": {
+        "projectile_type": "attack_wave",
+        "projectile_speed": 1.5,
+        "projectile_lifetime": 500,
+        "dmg": 1
+    },
+    "": {
+        "projectile_type": "attack_wave",
+        "projectile_speed": 1.5,
+        "projectile_lifetime": 500,
+        "dmg": 1
+    },
+    "diamondSword": {
+        "projectile_type": "attack_wave",
+        "projectile_speed": 1.5,
+        "projectile_lifetime": 500,
+        "dmg": 1
+    },
+    "ironPickaxe": {
+        "projectile_type": "attack_wave",
+        "projectile_speed": 1.5,
+        "projectile_lifetime": 500,
+        "dmg": 1
+    },
+    "goldenSpear": {
         "projectile_type": "attack_wave",
         "projectile_speed": 1.5,
         "projectile_lifetime": 500,
         "dmg": 1
     }
+
+
+
 }
 PLAYER_STATS = {
     "speed": 1,
